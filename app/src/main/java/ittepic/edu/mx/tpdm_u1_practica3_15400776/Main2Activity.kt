a8aefd5a6e210aaa2ebfc734c817746d7fd367d0
johicabreraas <johicabreraas@ittepic.edu.mx>
2019-09-25 07:39:23 -0600

    a

---
+++ app/src/main/java/ittepic/edu/mx/tpdm_u1_practica3_15400776/Main2Activity.kt
@@ -0,0 +1,12 @@
+package ittepic.edu.mx.tpdm_u1_practica3_15400776
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
